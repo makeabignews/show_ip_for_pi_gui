901a0de271d351b6fd52640837fe8e1f74e4eeeb
makeabignews <makeabignews@sina.com>
2017-11-12 22:36:35 +0800

    first

---
+++ mainwindow.py
@@ -0,0 +1,25 @@
+from PyQt4 import uic
+import socket
+
+( Ui_MainWindow, QMainWindow ) = uic.loadUiType( 'mainwindow.ui' )
+
+class MainWindow ( QMainWindow ):
+    """MainWindow inherits QMainWindow"""
+    def get_host_ip(self):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(('8.8.8.8', 80))
+            ip = s.getsockname()[0]
+        finally:
+            s.close()
+        return ip
+
+    def __init__ ( self, parent = None ):
+        QMainWindow.__init__( self, parent )
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi( self )
+        self.ui.label_ip.setText("ip:%s"%(self.get_host_ip()))
+        
+
+    def __del__ ( self ):
+        self.ui = None
